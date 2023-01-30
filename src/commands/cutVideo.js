62e02cf55bb6d1994d1c602a41ec81e4d7d333c0
Mayk Brito <maykbrito@gmail.com>
2023-01-30 17:12:12 -0300

    refactor cutVideo

--- src/commands/cutVideo.js
+++ src/commands/cutVideo.js
@@ -7,7 +7,6 @@ module.exports = {
   name: 'cutVideo',
   description: 'From given video file, will cut init and end part',
   async run(toolbox) {
-    console.log(toolbox)
     const {
       parameters: {
         first,
@@ -33,13 +32,13 @@ module.exports = {
 
     try {
       const outputFileName = removeCommonExtensionsFromFile(first)
-      await execSync(
+      execSync(
         `ffmpeg -y -ss "${start}" -t "${end}" -i "${first}" -c copy "${outputFileName}-part.mp4"`
       )
-      toolbox.print.success('Done!')
-    } catch (error) {
-      console.log(error)
-      toolbox.print.error('Something went wrong!')
+      success('Done!')
+    } catch (err) {
+      console.log(err)
+      error('Something went wrong!')
     }
   },
 }
