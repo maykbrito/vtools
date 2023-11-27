4cd8cda70af05613637730d7842e2f501a915680
Mayk Brito <maykbrito@gmail.com>
2023-11-27 12:33:16 -0300

    getExtension

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,14 @@
 const removeCommonExtensionsFromFile = (fileName) => {
-  const exts = ['.mp4', '.mkv']
+  const exts = ['.mp4', '.mkv', 'wav', 'mp3']
 
   return exts.reduce((acc, ext) => {
     return acc.replace(ext, '')
   }, fileName)
 }
 
+const getExtension = (file) => file.slice(-3)
+
 module.exports = {
+  getExtension,
   removeCommonExtensionsFromFile,
 }
